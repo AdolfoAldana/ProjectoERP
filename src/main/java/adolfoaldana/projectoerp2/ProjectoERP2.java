5bc7af70bb9b324bb925417253621c6baed4dbd1
aldan <aldan@DESKTOP-J4E72CM>
2023-09-09 20:04:09 -0600

    Prueba

---
+++ src/main/java/adolfoaldana/projectoerp2/ProjectoERP2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package adolfoaldana.projectoerp2;
+
+/**
+ *
+ * @author aldan
+ */
+public class ProjectoERP2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Pancho");
+    }
+}
